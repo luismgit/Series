330665af29206afca6f84d50cf7cdd067fa155db
luismgit <35255291+luismgit@users.noreply.github.com>
2018-02-22 11:12:28 +0100

    commit 22-02-18 , anulado el anteror

--- app/src/main/java/com/example/luis/series/actividades/TabActivity.java
+++ app/src/main/java/com/example/luis/series/actividades/TabActivity.java
@@ -86,8 +86,8 @@ SeriesFragment.OnFragmentInteractionListener,FavoritosFragment.OnFragmentInterac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab);
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
